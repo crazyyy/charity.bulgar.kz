e1bc2ef791156138f5eedcd6e3ea44654a8fcf7e
crazyyy <crazyyy@gmail.com>
2016-01-04 15:31:28 +0200

    fix contact page

--- wordpress/wp-content/themes/wp-conamen/page-contacts.php
+++ wordpress/wp-content/themes/wp-conamen/page-contacts.php
@@ -3,34 +3,25 @@
     <?php if (function_exists('easy_breadcrumbs')) easy_breadcrumbs(); ?>
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-      <?php if( have_rows('employe') ): ?>
+      <table class="contact-table">
+        <?php if( have_rows('employe') ): while ( have_rows('employe') ) : the_row(); ?>
 
-        <table class="contact-table">
-          <?php while( have_rows('employe') ): the_row();
-            // vars
-            $photo = get_sub_field('photo');
-            $name = get_sub_field('name');
-            $post = get_sub_field('post');
-            $email = get_sub_field('email');
-            $phone = get_sub_field('phone');
-          ?>
             <tr>
               <td>
-                <?php if( $photo ): ?>
-                  <img src="<?php echo $photo; ?>" alt="<?php echo $name; ?>">
+                <?php if(  get_sub_field('photo') ): ?>
+                  <img src="<?php the_sub_field('photo'); ?>" alt="<?php the_sub_field('name'); ?>">
                 <?php endif; ?>
               </td>
               <td>
-                <?php echo $name; ?><br>
-                <?php echo $post; ?><br>
-                email: <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a><br>
-                Mobil: <a href="tel:<?php echo $phone; ?>"><?php echo $phone; ?></a>
+                <?php the_sub_field('name'); ?><br>
+                <?php the_sub_field('post'); ?><br>
+                email: <a href="mailto:<?php echo $email; ?>"><?php the_sub_field('email'); ?></a><br>
+                Mobil: <a href="tel:<?php the_sub_field('phone'); ?>"><?php the_sub_field('phone'); ?></a>
               </td>
             </tr>
-          <?php endwhile; ?>
-        </table>
 
-      <?php endif; ?>
+        <?php endwhile; else : endif; ?>
+      </table>
 
     </article>
   <?php endwhile; else: // If 404 page error ?>
