cb9b5e133fb3aebbad0cc42be97b54bd4d1b5a55
Vitalii Antoniuk <va@jelasric.com>
2018-12-16 12:32:08 +0200

    close and archive project

--- wordpress/wp-content/themes/wp-conamen/category.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-<?php if (function_exists('easy_breadcrumbs')) easy_breadcrumbs(); ?>
-  <article>
-    <?php get_template_part('loop'); ?>
-    <?php get_template_part('pagination'); ?>
-  </article>
-<?php get_footer(); ?>
--- wordpress/wp-content/themes/wp-conamen/index.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-  <article>
-    <h1 class="inner-title"><?php _e( 'Latest Posts', 'wpeasy' ); ?></h1>
-    <?php get_template_part('loop'); ?>
-    <?php get_template_part('pagination'); ?>
-  </article>
-<?php get_footer(); ?>
